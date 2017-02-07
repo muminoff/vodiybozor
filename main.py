e679ef0fa2af7dc7c633cd0d5be30ccbb9949848
Sardor Muminov <sardor@insilicogen.com>
2017-02-07 18:13:33 +0900

    Fix typo

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ setattr(bot, 'pool', pool)
 
 if __name__ == '__main__':
     # loop.run_until_complete(run_bot())
-    bot.run_webhook(url=os.environ.get('APP_URL') + 'webhook')
+    bot.run_webhook(webhook_url=os.environ.get('APP_URL') + 'webhook')
