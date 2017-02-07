33c3a44beb8132368ca734b78299e8f1077e94d2
Sardor Muminov <sardor@insilicogen.com>
2017-02-07 10:19:52 +0900

    Update `smartphones` table

--- schema/smartphones.sql
+++ schema/smartphones.sql
@@ -4,6 +4,11 @@ CREATE TABLE smartphones (
     name text,
     year integer,
     price numeric,
+    mill text,
+    contact bigint,
+    username text,
+    image_url text,
+    is_sold bool DEFAULT false,
     is_published bool DEFAULT false,
     created timestamp without time zone DEFAULT timezone('Asia/Tashkent'::text, now())
 );
