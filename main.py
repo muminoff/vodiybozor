c8d079b703fc9a49c6457dcfabdaced8bbc6766e
muminoff <smuminov@gmail.com>
2017-02-08 23:22:03 +0900

    Add gunicorn

--- main.py
+++ main.py
@@ -16,11 +16,21 @@ async def make_pool():
     return await create_pool(dsn=dsn, min_size=10, max_size=20)
 
 
+# Main loop
 loop = asyncio.get_event_loop()
+
+# Make pool
 pool = loop.run_until_complete(make_pool())
 setattr(bot, 'pool', pool)
 
+# Webhook
+webhook_url = os.environ.get('APP_URL') + 'webhook'
+loop.run_until_complete(bot.set_webhook(webhook_url))
 
-if __name__ == '__main__':
-    # loop.run_until_complete(run_bot())
-    bot.run_webhook(webhook_url=os.environ.get('APP_URL') + 'webhook')
+# App
+app = bot.create_webhook_app('webhook', loop)
+
+
+# if __name__ == '__main__':
+#     # loop.run_until_complete(run_bot())
+#     bot.run_webhook(webhook_url=os.environ.get('APP_URL') + 'webhook')
