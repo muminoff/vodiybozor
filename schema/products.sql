0ba467d69d7585c93374ed1e2c8e520997928cc8
Sardor Muminov <sardor@insilicogen.com>
2017-02-08 17:17:57 +0900

    Update products schema

--- schema/products.sql
+++ schema/products.sql
@@ -2,15 +2,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS products (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     type uuid REFERENCES product_types(id),
-    name text,
-    year integer,
-    price numeric,
-    mill text,
-    contact bigint,
-    username text,
-    image_url text,
+    data jsonb,
     is_sold bool DEFAULT false,
     is_published bool DEFAULT false,
     written_by bigint REFERENCES users(id),
     created timestamp without time zone DEFAULT timezone('Asia/Tashkent'::text, now())
 );
+    /* name text, */
+    /* year integer, */
+    /* price numeric, */
+    /* mill text, */
+    /* contact bigint, */
+    /* username text, */
+    /* image_url text, */
