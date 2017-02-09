3a0036182227cbc6eca73d744e2cd881cc051f6c
muminoff <smuminov@gmail.com>
2017-02-09 23:41:37 +0900

    Add aioredis

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import asyncio
 import uvloop
 
 # Asyncpg
-from asyncpg import create_pool
+from asyncpg import create_pool as create_pg_pool
+
+# Aioredis
+from aioredis import create_pool as create_redis_pool
 
 # Bot
 from bot import bot
 
 # Misc
 import os
+from urllib.parse import urlparse
 
 # Use uvloop
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
@@ -23,16 +27,35 @@ async def run_bot():
 
 async def make_pg_pool():
     dsn = os.environ.get('DATABASE_URL')
-    return await create_pool(dsn=dsn, min_size=1, max_size=2)
+    return await create_pg_pool(
+            dsn=dsn,
+            min_size=1,
+            max_size=2)
 
+
+async def make_redis_pool():
+    redis_url = os.environ.get('REDIS_URL')
+    url = urlparse(redis_url)
+    return await create_redis_pool(
+            (url.hostname, url.port),
+            password=url.password,
+            minsize=1,
+            maxsize=2)
+
+# Main event loop
+loop = asyncio.get_event_loop()
 
 # Postgres connection pool
-loop = asyncio.get_event_loop()
 pg_pool = loop.run_until_complete(make_pg_pool())
 setattr(bot, 'pg_pool', pg_pool)
 
+redis_pool = loop.run_until_complete(make_redis_pool())
+setattr(bot, 'redis_pool', redis_pool)
 
 
 if __name__ == '__main__':
-    loop.run_until_complete(run_bot())
-    # bot.run_webhook(os.environ.get('APP_URL') + 'webhook')
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == 'loop':
+        loop.run_until_complete(run_bot())
+    else:
+        bot.run_webhook(os.environ.get('APP_URL') + 'webhook')
