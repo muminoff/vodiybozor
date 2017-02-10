046bf579ade39a90ec705ea36a423fa5df682128
Sardor Muminov <sardor@insilicogen.com>
2017-02-10 20:07:31 +0900

    Make simple unknown page

--- commands/basic.py
+++ commands/basic.py
@@ -80,22 +80,27 @@ async def process_stop_command(chat, match, logger):
 
 async def process_unknown_command(chat, match, logger):
     question = format_text('''
-    {name}, мен ботман. Бунақа гапларни тўғриси тушунмайман. Мен фақат чой дамлайман холос. 😃
+    {name}, қизиқиш билдирганингиз учун раҳмат.
 
-    Балки, админларга бирор гапингиз бордир?
+    Ҳозирда бу бўлим дастурлаштириляпти.
     ''')
-    keyboard = [
-        ['👮🏻 Админ керак', '📃 Менюни кўрмоқчиман'],
-    ]
-    reply_keyboard_markup = {
-        'keyboard': keyboard,
-        'resize_keyboard': True,
-        'one_time_keyboard': True
-    }
+    # question = format_text('''
+    # {name}, мен ботман. Бунақа гапларни тўғриси тушунмайман. Мен фақат чой дамлайман холос. 😃
+
+    # Балки, админларга бирор гапингиз бордир?
+    # ''')
+    # keyboard = [
+    #     ['👮🏻 Админ керак', '📃 Менюни кўрмоқчиман'],
+    # ]
+    # reply_keyboard_markup = {
+    #     'keyboard': keyboard,
+    #     'resize_keyboard': True,
+    #     'one_time_keyboard': True
+    # }
 
     logger.info('%s unknown requested by', chat.sender)
     await chat.send_text(
         question.format(name=chat.sender['first_name']),
         parse_mode='Markdown',
-        disable_web_page_preview=True,
-        reply_markup=json.dumps(reply_keyboard_markup))
+        disable_web_page_preview=True)
+        # reply_markup=json.dumps(reply_keyboard_markup))
