cc652f36ab4bd1917493727f74ea844b99ccc988
Sardor Muminov <sardor@insilicogen.com>
2017-02-10 16:58:42 +0900

    Fix import

--- commands/basic.py
+++ commands/basic.py
@@ -2,6 +2,7 @@
 from queries.users import user_exists
 from queries.users import insert_user
 from queries.users import deactivate_user
+from queries.users import get_admins
 
 # Helpers
 from utils.helpers import format_text
