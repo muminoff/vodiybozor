177f126adae53ac66946a3e46a2c8ad27363e4f5
Sardor Muminov <sardor@insilicogen.com>
2017-02-10 14:37:53 +0900

    Update contact

--- commands/contact.py
+++ commands/contact.py
@@ -1,10 +1,22 @@
+# Queries
+from queries.users import get_admins
+
 # Helpers
 from utils.helpers import format_text
 
 
 async def process_contact_command(chat, match, logger):
-    info = format_text('''
-    Админ билан боғланиш
-    ''')
     logger.info('%s contact requested by', chat.sender)
-    await chat.send_text(info)
+
+    contacts = format_text('''
+    *Админлар:*
+
+    {admins}
+    ''')
+
+    admins = []
+    for admin in await get_admins(chat.bot.pg_pool):
+        admins.append('@' + admin['username'])
+
+    admins_text = '\n'.join(admins)
+    await chat.send_text(contacts.format(admins=admins_text))
