a4d7ef032a9405bde0b9c41543e422cbb58f287a
Sardor Muminov <sardor@insilicogen.com>
2017-02-16 10:51:01 +0900

    Typo

--- commands/contacts.py
+++ commands/contacts.py
@@ -8,7 +8,7 @@ from queries import insert_contact
 async def process_contact(chat, match, logger):
     logger.info("Getting contact from %s", chat.sender)
     logger.info(chat.message['contact'])
-    await insert_contact(chat.bot.pg_pool, chat.message['contact']):
+    await insert_contact(chat.bot.pg_pool, chat.message['contact'])
     info = format_text('''
     Телефон рақам қабул қилинди. Раҳмат.
     ''')
