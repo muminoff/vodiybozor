d50b8abc2dc6374eb53cf9ecd1bcf52be5a4a756
muminoff <smuminov@gmail.com>
2017-02-19 15:35:50 +0900

    Send photo back

--- bot.py
+++ bot.py
@@ -157,6 +157,8 @@ async def make_self_ad(chat, match):
 
 @bot.handle("photo")
 async def get_photo(chat, match):
+    file_id = chat.message['photo'][1]['file_id']
+    await chat.send_photo(file_id)
 
     # if not await user_has_any_draft(chat.bot.pg_pool, chat.sender.get('id')):
     #     info = format_text('''
@@ -173,8 +175,8 @@ async def get_photo(chat, match):
     # category_id = await get_draft_category(chat.bot.pg_pool, chat.sender.get('id'))
     # draft = await get_draft(chat.bot.pg_pool, chat.sender.get('id'), category_id)
     # ad = await make_ad_from_draft(draft)
-    url = await process_photo(chat, match, logger)
-    await chat.send_photo(url, caption='')
+    # url = await process_photo(chat, match, logger)
+    # await chat.send_photo(url, caption='')
 
 
 @bot.handle("contact")
