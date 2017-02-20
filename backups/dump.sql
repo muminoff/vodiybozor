458b1cb7a22ff8c707bcdd91b68d72fb1ab5117a
Sardor Muminov <sardor@insilicogen.com>
2017-02-20 10:17:20 +0900

    DB dump

---
+++ backups/dump.sql
@@ -0,0 +1,159 @@
+COPY categories (id, name, created) FROM stdin;
+1	Авто-улов	2017-02-15 02:21:41.739717
+2	Кўчмас-мулк	2017-02-15 02:21:41.739717
+3	Маиший техника	2017-02-15 02:21:41.739717
+4	Уй-рўзғор буюмлари	2017-02-15 02:21:41.739717
+5	Кийим-кечак	2017-02-15 02:21:41.739717
+6	Спорт анжомлари	2017-02-15 02:21:41.739717
+7	Телефон	2017-02-15 02:21:41.739717
+\.
+
+COPY "users" ("id", "first_name", "last_name", "username", "is_admin", "is_active", "joined") FROM stdin;
+160546867	Qudratillo		PIRATEDRAKE	t	t	2017-02-10 23:03:16.317933+00
+260951612	ВЭБозор	Админ	Vebozoradmin	t	f	2017-02-10 11:23:53.408407+00
+167188863	(( Imrоnchik	))		f	t	2017-02-14 22:57:58.197157+00
+256412	Иззатбек	Мусаев	andbozor	t	t	2017-02-10 21:41:41.684446+00
+292814220	12 22			f	t	2017-02-14 23:04:15.323614+00
+69508059	Шах ан Шах		Sefevie	f	f	2017-02-10 22:29:58.965547+00
+882017	Jafar		jafarjon	f	t	2017-02-11 13:30:04.609239+00
+167385938	Farangiz			f	t	2017-02-11 18:14:42.249224+00
+1438222	10:11	ÏšŁøM ßęK TDŸÛÛ		f	t	2017-02-11 22:04:49.884794+00
+226467064	Sanjeek	Scofield	SanjeekScofield	f	f	2017-02-12 01:05:35.458282+00
+255710274	I	Muhammadali	Cordiodoctor	f	t	2017-02-10 09:41:28.867114+00
+231826623	Алмаз	Муйдинов		f	t	2017-02-12 05:00:09.567127+00
+113363905	Madinabonu	Agzamova		f	t	2017-02-12 06:31:28.56616+00
+79861994	АБУ ХИКМАТ			f	t	2017-02-07 20:54:25.93352+00
+8092351	Kozimjon		Kozimjon707	f	f	2017-02-07 20:59:13.903749+00
+287724279	Jorabek	Yusupov	Jorabek_Yusupov	f	f	2017-02-12 21:09:11.959526+00
+1738671	Андижанец		alibahodur	t	t	2017-02-07 13:40:07.663531+00
+44133074	Djo	Black	anjanlik	t	t	2017-02-07 14:02:22.806306+00
+132655850	Abror Akramovich			f	t	2017-02-10 22:42:10.341472+00
+303204903	Izzatillo	OOO"BOZ FARM"	OOO_BOZ_FARM	f	t	2017-02-13 14:31:24.461096+00
+38564275	Шухратбек		otajonov11	f	f	2017-02-10 12:39:14.98385+00
+285376238	Marhabo			f	t	2017-02-10 18:26:39.106203+00
+188212873	Ulugbek	Ulugbek		f	t	2017-02-13 18:17:57.482014+00
+198735717	Бону			f	t	2017-02-13 17:41:22.197587+00
+97130731	OYBEK			f	t	2017-02-13 20:19:44.241456+00
+198576829	♡♥♡МММ♡♥♡			f	t	2017-02-13 20:45:04.652079+00
+147067071	Abdujalil			f	t	2017-02-13 22:05:15.06992+00
+308731616	23903			f	t	2017-02-14 09:17:32.200152+00
+315858961	Qosimov			f	t	2017-02-14 11:21:52.475324+00
+220891996	UzMaN		Anjansky	f	t	2017-02-14 20:20:26.129639+00
+77348597	S.B.R			f	t	2017-02-15 08:35:26.042238+00
+77765328	M.R		MpurposeR	f	t	2017-02-15 08:54:53.850062+00
+332902324	+998977777777		SIDIQJONOV	f	t	2017-02-15 09:18:46.209162+00
+209987951	PRINCE 707		Olganimda_izlama_yorim	f	t	2017-02-15 09:27:20.755437+00
+325990467	To'lqin	Nuraddinov		f	t	2017-02-15 09:38:26.512008+00
+173051862	Black	Star		f	t	2017-02-15 09:47:51.585907+00
+88211257	Javlonbek		JavlonbekToshmatov	f	t	2017-02-15 10:17:53.492923+00
+284153855	©®7			f	t	2017-02-15 10:29:02.647561+00
+289865361	Anvar	Tuxtaev		f	t	2017-02-15 11:43:52.787708+00
+282067233	Avulova	Dildora		f	t	2017-02-15 11:45:10.385495+00
+174431626	Eldorbek	Abdurashidov	Eldorbek94	f	t	2017-02-15 11:52:30.297559+00
+204507280	007		futbol90	f	t	2017-02-15 12:19:06.096626+00
+284133535	Trap bassss			f	t	2017-02-15 13:05:18.390688+00
+213632408	0708	.		f	t	2017-02-15 13:08:33.571061+00
+130601559	RAXMATULLO			f	t	2017-02-09 20:44:35.40357+00
+182691886	$Ibrokhim	Ergashev	Ibrokhim_06	f	t	2017-02-15 11:44:51.337064+00
+137587999	TIMUR	Timur		f	t	2017-02-16 09:46:47.767654+00
+126235361	M1O			f	t	2017-02-16 10:54:02.268356+00
+137684218	Ислом	Ашурв		f	t	2017-02-16 15:32:38.929456+00
+202970222	Smart	Boy	TraderAdmin	f	t	2017-02-15 08:49:15.121548+00
+90276273	+998945677744		MrKhann	f	t	2017-02-18 20:09:50.509494+00
+250826346	K		KoMa1222	f	t	2017-02-18 22:02:59.815151+00
+376455305	...			f	t	2017-02-18 22:08:45.328039+00
+163624943	Black ☆		Wetyu45	f	t	2017-02-18 22:10:24.707169+00
+55160536	Dobriy sinko			f	t	2017-02-18 22:19:01.806851+00
+201240386	Abdurazzoq	Xasanov		f	t	2017-02-18 22:26:56.641523+00
+322854740	AZIZ		Muxsinjonov	f	t	2017-02-18 22:28:44.554406+00
+183705829	Toxtasin	Raximov		f	t	2017-02-18 22:36:15.118845+00
+2669804	Азизбек	Абдумаликов	Abdumalikoov	f	t	2017-02-18 22:39:31.341621+00
+145237218	Халимжон	6767		f	t	2017-02-18 22:42:49.143058+00
+24139624	+998977777577			f	t	2017-02-18 22:44:10.827526+00
+2843309	Tog'enmanu	Tanimadimi		f	t	2017-02-18 22:54:02.162217+00
+248665004	ÀŁÍ😉			f	t	2017-02-18 22:54:40.687951+00
+223577213	БЕМ И ЖЕК	Bem & Jek		f	t	2017-02-18 23:01:27.142096+00
+166264856	**8583__		uztelegramist	f	t	2017-02-18 23:02:21.262127+00
+281169150	Олимов			f	t	2017-02-18 23:19:41.134557+00
+200243917	Хамдам	Хаджиев		f	t	2017-02-18 23:43:16.552666+00
+310426145	Muxammad	Rasul		f	t	2017-02-19 00:09:48.455073+00
+233665340	Umarganov	_95		f	t	2017-02-19 00:32:55.239079+00
+360703068	Abdug'affor	Sharipov		f	t	2017-02-19 01:51:26.962481+00
+195257972	Muzaffar			f	t	2017-02-19 01:57:24.302074+00
+46597370	Jaxongir		YJaxongir	f	t	2017-02-19 04:34:00.198606+00
+164397833	Imomjon	Karimov		f	t	2017-02-19 05:27:18.432998+00
+51198967	اسلام		islam_khudoyberdiyev	f	t	2017-02-19 06:48:22.607121+00
+327915404	Рихсивой	С		f	t	2017-02-19 07:21:32.725513+00
+335609184	Шарипов		Sharipov2801	f	t	2017-02-19 09:54:05.450984+00
+233519215	Shahzod			f	t	2017-02-19 09:58:29.549857+00
+187296723	SWAT			f	t	2017-02-19 10:15:37.545935+00
+284093472	Dilmirod			f	t	2017-02-19 10:29:17.418241+00
+219780807	Idieal	Rich		f	t	2017-02-19 11:47:46.632242+00
+254264062	Sanjarbek			f	t	2017-02-19 12:57:14.101723+00
+326534061	Alik	AlikoF		f	t	2017-02-19 16:55:33.661556+00
+92165988	(🔚Abror_Bek🔜)		officaliiiiAbrorbek	f	t	2017-02-19 20:02:18.367608+00
+308924645	Atlant			f	t	2017-02-19 21:25:24.666787+00
+310088711	Uralov	Asliddin		f	t	2017-02-19 21:41:32.945581+00
+286388221	Shaxzod			f	t	2017-02-18 21:59:52.652205+00
+2902951	Azizbek♬	ZyzzBro	AloneRomeo	f	t	2017-02-20 00:51:28.773145+00
+56781796	Sardor		muminofff	f	t	2017-02-07 07:23:19.290876+00
+\.
+
+COPY "contacts" ("user_id", "phone_number", "created") FROM stdin;
+56781796	821035027155	2017-02-17 06:47:20.648317+00
+163624943	998933963730	2017-02-18 22:11:28.473231+00
+322854740	998917794455	2017-02-18 22:34:14.856721+00
+281169150	998936497773	2017-02-18 23:20:20.197299+00
+310426145	+905347139609	2017-02-19 00:12:15.046842+00
+233665340	+79522035707	2017-02-19 00:34:19.84807+00
+287724279	998901400075	2017-02-19 08:51:27.741157+00
+310088711	998909890624	2017-02-19 21:42:15.484012+00
+\.
+
+
+
+COPY "visitors" ("id", "timestamp", "user_id", "message") FROM stdin;
+1	2017-02-17 06:42:22.05506+00	56781796	{"chat": {"id": 56781796, "type": "private", "username": "muminofff", "first_name": "Sardor"}, "date": 1487295718, "from": {"id": 56781796, "username": "muminofff", "first_name": "Sardor"}, "text": "qwe", "message_id": 3570}
+2	2017-02-17 06:50:16.286115+00	56781796	{"chat": {"id": 56781796, "type": "private", "username": "muminofff", "first_name": "Sardor"}, "date": 1487296215, "from": {"id": 56781796, "username": "muminofff", "first_name": "Sardor"}, "text": "test", "message_id": 3574}
+3	2017-02-17 11:57:11.454158+00	260951612	{"chat": {"id": 260951612, "type": "private", "username": "Vebozoradmin", "last_name": "Админ", "first_name": "ВЭБозор"}, "date": 1487314631, "from": {"id": 260951612, "username": "Vebozoradmin", "last_name": "Админ", "first_name": "ВЭБозор"}, "text": "Обуна бўлмоқчиман", "message_id": 3580}
+4	2017-02-17 15:11:43.396046+00	56781796	{"chat": {"id": 56781796, "type": "private", "username": "muminofff", "first_name": "Sardor"}, "date": 1487326303, "from": {"id": 56781796, "username": "muminofff", "first_name": "Sardor"}, "text": "kuraman", "message_id": 3584}
+5	2017-02-17 15:12:03.50064+00	56781796	{"chat": {"id": 56781796, "type": "private", "username": "muminofff", "first_name": "Sardor"}, "date": 1487326323, "from": {"id": 56781796, "username": "muminofff", "first_name": "Sardor"}, "text": "hech nima", "message_id": 3588}
+6	2017-02-18 12:25:24.70672+00	260951612	{"chat": {"id": 260951612, "type": "private", "username": "Vebozoradmin", "last_name": "Админ", "first_name": "ВЭБозор"}, "date": 1487402724, "from": {"id": 260951612, "username": "Vebozoradmin", "last_name": "Админ", "first_name": "ВЭБозор"}, "text": "📺 Маиший техника", "message_id": 3596}
+7	2017-02-18 16:48:54.837236+00	56781796	{"chat": {"id": 56781796, "type": "private", "username": "muminofff", "first_name": "Sardor"}, "date": 1487418532, "from": {"id": 56781796, "username": "muminofff", "first_name": "Sardor"}, "text": "Алаоадп", "message_id": 3600}
+8	2017-02-18 20:10:16.645176+00	90276273	{"chat": {"id": 90276273, "type": "private", "username": "MrKhann", "first_name": "+998945677744"}, "date": 1487430616, "from": {"id": 90276273, "username": "MrKhann", "first_name": "+998945677744"}, "text": "Олмоқчиман", "message_id": 3610}
+10	2017-02-18 20:34:11.746946+00	90276273	{"chat": {"id": 90276273, "type": "private", "username": "MrKhann", "first_name": "+998945677744"}, "date": 1487432051, "from": {"id": 90276273, "username": "MrKhann", "first_name": "+998945677744"}, "text": "Обуналарни кўрмоқчиман", "message_id": 3620}
+11	2017-02-18 22:09:23.435347+00	376455305	{"chat": {"id": 376455305, "type": "private", "first_name": "..."}, "date": 1487437763, "from": {"id": 376455305, "first_name": "..."}, "text": "Олмоқчиман", "message_id": 3644}
+12	2017-02-18 22:11:01.867103+00	163624943	{"chat": {"id": 163624943, "type": "private", "username": "Wetyu45", "first_name": "Black ☆"}, "date": 1487437861, "from": {"id": 163624943, "username": "Wetyu45", "first_name": "Black ☆"}, "text": "Олмоқчиман", "message_id": 3654}
+13	2017-02-18 22:13:22.586757+00	163624943	{"chat": {"id": 163624943, "type": "private", "username": "Wetyu45", "first_name": "Black ☆"}, "date": 1487438002, "from": {"id": 163624943, "username": "Wetyu45", "first_name": "Black ☆"}, "text": "Обуна бўлмоқчиман", "message_id": 3660}
+14	2017-02-18 22:13:59.708332+00	163624943	{"chat": {"id": 163624943, "type": "private", "username": "Wetyu45", "first_name": "Black ☆"}, "date": 1487438039, "from": {"id": 163624943, "username": "Wetyu45", "first_name": "Black ☆"}, "text": "Обуналарни кўрмоқчиман", "message_id": 3668}
+15	2017-02-18 22:19:29.472425+00	55160536	{"chat": {"id": 55160536, "type": "private", "first_name": "Dobriy sinko"}, "date": 1487438369, "from": {"id": 55160536, "first_name": "Dobriy sinko"}, "text": "Toshkent", "message_id": 3678}
+16	2017-02-18 22:33:35.620801+00	322854740	{"chat": {"id": 322854740, "type": "private", "username": "Muxsinjonov", "first_name": "AZIZ"}, "date": 1487439215, "from": {"id": 322854740, "username": "Muxsinjonov", "first_name": "AZIZ"}, "text": "👕 Кийим-кечак", "message_id": 3692}
+17	2017-02-18 22:36:36.46884+00	183705829	{"chat": {"id": 183705829, "type": "private", "last_name": "Raximov", "first_name": "Toxtasin"}, "date": 1487439396, "from": {"id": 183705829, "last_name": "Raximov", "first_name": "Toxtasin"}, "text": "андижон", "message_id": 3700}
+18	2017-02-18 22:40:00.404596+00	2669804	{"chat": {"id": 2669804, "type": "private", "username": "Abdumalikoov", "last_name": "Абдумаликов", "first_name": "Азизбек"}, "date": 1487439600, "from": {"id": 2669804, "username": "Abdumalikoov", "last_name": "Абдумаликов", "first_name": "Азизбек"}, "text": "Элон бермокчиман", "message_id": 3704}
+19	2017-02-18 22:40:25.236391+00	2669804	{"chat": {"id": 2669804, "type": "private", "username": "Abdumalikoov", "last_name": "Абдумаликов", "first_name": "Азизбек"}, "date": 1487439625, "from": {"id": 2669804, "username": "Abdumalikoov", "last_name": "Абдумаликов", "first_name": "Азизбек"}, "text": "Ок", "message_id": 3706}
+20	2017-02-18 22:41:56.539249+00	183705829	{"chat": {"id": 183705829, "type": "private", "last_name": "Raximov", "first_name": "Toxtasin"}, "date": 1487439716, "from": {"id": 183705829, "last_name": "Raximov", "first_name": "Toxtasin"}, "text": "Телефон рақам юбориш", "message_id": 3708}
+21	2017-02-18 22:42:31.433876+00	183705829	{"chat": {"id": 183705829, "type": "private", "last_name": "Raximov", "first_name": "Toxtasin"}, "date": 1487439751, "from": {"id": 183705829, "last_name": "Raximov", "first_name": "Toxtasin"}, "text": "удалит бот", "message_id": 3710}
+23	2017-02-18 23:20:04.148266+00	281169150	{"chat": {"id": 281169150, "type": "private", "first_name": "Олимов"}, "date": 1487442003, "from": {"id": 281169150, "first_name": "Олимов"}, "text": "Fergana", "message_id": 3743}
+24	2017-02-18 23:20:44.749488+00	281169150	{"chat": {"id": 281169150, "type": "private", "first_name": "Олимов"}, "date": 1487442044, "from": {"id": 281169150, "first_name": "Олимов"}, "text": "Qacon iwga tuwadi", "message_id": 3747}
+25	2017-02-18 23:21:15.632545+00	281169150	{"chat": {"id": 281169150, "type": "private", "first_name": "Олимов"}, "date": 1487442075, "from": {"id": 281169150, "first_name": "Олимов"}, "text": "Toshkent", "message_id": 3749}
+26	2017-02-18 23:30:34.481017+00	281169150	{"chat": {"id": 281169150, "type": "private", "first_name": "Олимов"}, "date": 1487442633, "from": {"id": 281169150, "first_name": "Олимов"}, "text": "Rahmat", "message_id": 3751}
+27	2017-02-19 00:10:09.016752+00	310426145	{"chat": {"id": 310426145, "type": "private", "last_name": "Rasul", "first_name": "Muxammad"}, "date": 1487445008, "from": {"id": 310426145, "last_name": "Rasul", "first_name": "Muxammad"}, "text": "Олмоқчиман", "message_id": 3759}
+28	2017-02-19 00:13:16.724708+00	310426145	{"chat": {"id": 310426145, "type": "private", "last_name": "Rasul", "first_name": "Muxammad"}, "date": 1487445196, "from": {"id": 310426145, "last_name": "Rasul", "first_name": "Muxammad"}, "text": "Обуналарни кўрмоқчиман", "message_id": 3769}
+29	2017-02-19 00:34:03.513655+00	233665340	{"chat": {"id": 233665340, "type": "private", "last_name": "_95", "first_name": "Umarganov"}, "date": 1487446443, "from": {"id": 233665340, "last_name": "_95", "first_name": "Umarganov"}, "text": "Обуна бўлмоқчиман", "message_id": 3781}
+30	2017-02-19 00:47:49.62957+00	233665340	{"chat": {"id": 233665340, "type": "private", "last_name": "_95", "first_name": "Umarganov"}, "date": 1487447269, "from": {"id": 233665340, "last_name": "_95", "first_name": "Umarganov"}, "text": "Обуналарни кўрмоқчиман", "message_id": 3791}
+31	2017-02-19 01:52:14.94124+00	360703068	{"chat": {"id": 360703068, "type": "private", "last_name": "Sharipov", "first_name": "Abdug'affor"}, "date": 1487451134, "from": {"id": 360703068, "last_name": "Sharipov", "first_name": "Abdug'affor"}, "text": "Обуна бўлмоқчиман", "message_id": 3798}
+32	2017-02-19 01:52:35.594606+00	360703068	{"chat": {"id": 360703068, "type": "private", "last_name": "Sharipov", "first_name": "Abdug'affor"}, "date": 1487451155, "from": {"id": 360703068, "last_name": "Sharipov", "first_name": "Abdug'affor"}, "text": "Телефон рақам юбориш", "message_id": 3800}
+33	2017-02-19 01:57:29.703264+00	195257972	{"chat": {"id": 195257972, "type": "private", "first_name": "Muzaffar"}, "date": 1487451449, "from": {"id": 195257972, "first_name": "Muzaffar"}, "text": "alo", "message_id": 3804}
+34	2017-02-19 01:57:53.216864+00	195257972	{"chat": {"id": 195257972, "type": "private", "first_name": "Muzaffar"}, "date": 1487451473, "from": {"id": 195257972, "first_name": "Muzaffar"}, "text": "kormoqchiman", "message_id": 3806}
+35	2017-02-19 01:58:49.017415+00	195257972	{"chat": {"id": 195257972, "type": "private", "first_name": "Muzaffar"}, "date": 1487451528, "from": {"id": 195257972, "first_name": "Muzaffar"}, "text": "ali", "message_id": 3808}
+36	2017-02-19 01:58:51.602772+00	195257972	{"chat": {"id": 195257972, "type": "private", "first_name": "Muzaffar"}, "date": 1487451531, "from": {"id": 195257972, "first_name": "Muzaffar"}, "text": "alo", "message_id": 3810}
+37	2017-02-19 04:34:21.756245+00	46597370	{"chat": {"id": 46597370, "type": "private", "username": "YJaxongir", "first_name": "Jaxongir"}, "date": 1487460861, "from": {"id": 46597370, "username": "YJaxongir", "first_name": "Jaxongir"}, "text": "Kormoqchi edim", "message_id": 3815}
+38	2017-02-19 06:50:01.22672+00	51198967	{"chat": {"id": 51198967, "type": "private", "username": "islam_khudoyberdiyev", "first_name": "اسلام"}, "date": 1487469000, "from": {"id": 51198967, "username": "islam_khudoyberdiyev", "first_name": "اسلام"}, "text": "📱 Телефон", "message_id": 3835}
+39	2017-02-19 07:50:34.666277+00	327915404	{"chat": {"id": 327915404, "type": "private", "last_name": "С", "first_name": "Рихсивой"}, "date": 1487472634, "from": {"id": 327915404, "last_name": "С", "first_name": "Рихсивой"}, "text": "Водий бозори", "message_id": 3841}
+40	2017-02-19 08:51:16.343789+00	287724279	{"chat": {"id": 287724279, "type": "private", "username": "Jorabek_Yusupov", "last_name": "Yusupov", "first_name": "Jorabek"}, "date": 1487476276, "from": {"id": 287724279, "username": "Jorabek_Yusupov", "last_name": "Yusupov", "first_name": "Jorabek"}, "text": "📱 Телефон", "message_id": 3846}
+41	2017-02-19 09:55:27.719041+00	335609184	{"chat": {"id": 335609184, "type": "private", "username": "Sharipov2801", "first_name": "Шарипов"}, "date": 1487480127, "from": {"id": 335609184, "username": "Sharipov2801", "first_name": "Шарипов"}, "text": "/off", "entities": [{"type": "bot_command", "length": 4, "offset": 0}], "message_id": 3882}
+43	2017-02-19 21:42:07.209136+00	310088711	{"chat": {"id": 310088711, "type": "private", "last_name": "Asliddin", "first_name": "Uralov"}, "date": 1487522527, "from": {"id": 310088711, "last_name": "Asliddin", "first_name": "Uralov"}, "text": "Олмоқчиман", "message_id": 3929}
+44	2017-02-19 21:42:50.814419+00	310088711	{"chat": {"id": 310088711, "type": "private", "last_name": "Asliddin", "first_name": "Uralov"}, "date": 1487522570, "from": {"id": 310088711, "last_name": "Asliddin", "first_name": "Uralov"}, "text": "2678", "message_id": 3933}
+45	2017-02-19 21:43:15.034988+00	310088711	{"chat": {"id": 310088711, "type": "private", "last_name": "Asliddin", "first_name": "Uralov"}, "date": 1487522594, "from": {"id": 310088711, "last_name": "Asliddin", "first_name": "Uralov"}, "text": "Gbjjp", "message_id": 3935}
+46	2017-02-19 21:43:25.287845+00	310088711	{"chat": {"id": 310088711, "type": "private", "last_name": "Asliddin", "first_name": "Uralov"}, "date": 1487522605, "from": {"id": 310088711, "last_name": "Asliddin", "first_name": "Uralov"}, "text": "Vbhk", "message_id": 3937}
+\.
