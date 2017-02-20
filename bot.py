9d64590a0637e7b523476ea5fce93cd66cd9deb1
Sardor <smuminov@gmail.com>
2017-02-20 09:31:17 +0000

    Inline query

--- bot.py
+++ bot.py
@@ -115,6 +115,27 @@ async def attach_no_image_to_ad(chat, match):
     await attach_no_image_to_ad_command(chat, match, logger)
 
 
+@bot.inline
+async def inline(iq):
+    logger.info('%s searching for %s', iq.sender, iq.query)
+    message = {
+        "message_text": "test content",
+        "parse_mode": "markdown",
+        "disable_web_page_preview": True
+    }
+    results = [
+        {
+            "type": "article",
+            "id": "12",
+            "title": "test title",
+            "input_message_content": message,
+            "description": "test description",
+            "hide_url": True
+        }
+    ]
+    await iq.answer(results)
+
+
 @bot.command(r'Эълонларни кўрмоқчиман')
 @bot.command(r'elonlar')
 async def view_ads(chat, match):
